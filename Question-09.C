2bbc9091077c72ba11e128c80e77e915f826267d
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-28 08:48:20 +0530

    Update Question-09.C

--- Question-09.C
+++ Question-09.C
@@ -1,1 +1,30 @@
+// Write a program in C to count the digits of a given number using recursion.
+
+#include<stdio.h>
+int count(int);
+
+int count(int x)
+{
+        static int c = 0 ;
+        if(x>0)
+        {
+                c ++ ;
+                count(x/10);
+        }
+        
+        return c ;
+}
+
+
+int main()
+{
+    int x ;
+    printf("Enter a number : ");
+    scanf("%d", &x);
+    
+    printf(" count is %d",count(x));
+
+    
+    return 0;
+}
 
